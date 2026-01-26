286ff76a7cb7ca6d2a9d54f8dd0181e3f9d897b8
Drew Herron <herron@mailbox.org>
2026-01-25 18:51:03 -0700

    Add diagnostic output with log levels

---
+++ logging.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+// LogLevel represents the logging level
+type LogLevel int
+
+const (
+	LogLevelSilent LogLevel = iota
+	LogLevelError
+	LogLevelWarn  
+	LogLevelInfo
+	LogLevelDebug
+)
+
+var (
+	currentLogLevel LogLevel = LogLevelInfo
+	enableColors    bool     = true
+)
+
+func init() {
+	// Check environment variables for log configuration
+	if level := os.Getenv("KEYKAMMER_LOG_LEVEL"); level != "" {
+		switch strings.ToLower(level) {
+		case "silent":
+			currentLogLevel = LogLevelSilent
+		case "error":
+			currentLogLevel = LogLevelError
+		case "warn", "warning":
+			currentLogLevel = LogLevelWarn
+		case "info":
+			currentLogLevel = LogLevelInfo
+		case "debug":
+			currentLogLevel = LogLevelDebug
+		}
+	}
+	
+	// Check if colors should be disabled
+	if os.Getenv("NO_COLOR") != "" || os.Getenv("KEYKAMMER_NO_COLOR") != "" {
+		enableColors = false
+	}
+	
+	// Disable standard log prefixes for cleaner output
+	log.SetFlags(0)
+}
+
+// Production logging functions
+func logError(format string, args ...interface{}) {
+	if currentLogLevel >= LogLevelError {
+		if enableColors {
+			fmt.Printf("\033[31m[ERROR]\033[0m "+format+"\n", args...)
+		} else {
+			fmt.Printf("[ERROR] "+format+"\n", args...)
+		}
+	}
+}
+
+func logWarn(format string, args ...interface{}) {
+	if currentLogLevel >= LogLevelWarn {
+		if enableColors {
+			fmt.Printf("\033[33m[WARN]\033[0m "+format+"\n", args...)
+		} else {
+			fmt.Printf("[WARN] "+format+"\n", args...)
+		}
+	}
+}
+
+func logInfo(format string, args ...interface{}) {
+	if currentLogLevel >= LogLevelInfo {
+		if enableColors {
+			fmt.Printf("\033[36m[INFO]\033[0m "+format+"\n", args...)
+		} else {
+			fmt.Printf("[INFO] "+format+"\n", args...)
+		}
+	}
+}
+
+func logDebug(format string, args ...interface{}) {
+	if currentLogLevel >= LogLevelDebug {
+		if enableColors {
+			fmt.Printf("\033[35m[DEBUG]\033[0m "+format+"\n", args...)
+		} else {
+			fmt.Printf("[DEBUG] "+format+"\n", args...)
+		}
+	}
+}
+
+// User-facing messages (always shown regardless of log level)
+func userInfo(format string, args ...interface{}) {
+	fmt.Printf(format+"\n", args...)
+}
+
+func userError(format string, args ...interface{}) {
+	if enableColors {
+		fmt.Printf("\033[31m"+format+"\033[0m\n", args...)
+	} else {
+		fmt.Printf(format+"\n", args...)
+	}
+}
+
+func userSuccess(format string, args ...interface{}) {
+	if enableColors {
+		fmt.Printf("\033[32m"+format+"\033[0m\n", args...)
+	} else {
+		fmt.Printf(format+"\n", args...)
+	}
+}
